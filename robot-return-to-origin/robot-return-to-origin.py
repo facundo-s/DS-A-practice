7aff5f91399a01582da003242857b22c6d2c3439
Facundo Severi <f.severi@berkeley.edu>
2021-01-24 18:19:33 +0100

    Time: 72 ms (31.40%), Space: 14.6 MB (21.44%) - LeetHub

---
+++ robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,13 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        position=[0,0]
+        for move in moves:
+            if move =="R":
+                position[0]+=1
+            elif move=="L":
+                position[0]-=1
+            elif move=="U":
+                position[1]+=1
+            else:
+                position[1]-=1
+        return position[0]==0 and position[1]==0
