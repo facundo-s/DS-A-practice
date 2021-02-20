235660a6ae9e4cd93efe1a4b847c02dcfd4c6ea6
Facundo Severi <facundo.severi@gmail.com>
2021-02-20 12:55:54 +0100

    Time: 68 ms (79.01%), Space: 14.6 MB (59.29%) - LeetHub

--- remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
+++ remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -1,20 +1,31 @@
 class Solution:
     def removeDuplicates(self, S: str) -> str:
-        stack = [S]
-        while stack:
-            word = stack.pop()
-            if word == "":
-                break
-            last_letter = word[0]
-            for i in range(1,len(word)):
-                #iterate over all the letters to find a duplicate
-                #if a duplicate is found, remove it, add it to the stack
-                if word[i] == last_letter:
-                    stack.append(word[:i-1]+word[i+1:])
-                    break
-                last_letter = word[i]
+        stack = []
+        for char in S:
             if not stack:
-                #if after checking the word for duplicates, none is found, exit loop
-                break
-        return word
+                stack.append(char)
+                continue
+            if char == stack[-1]:
+                stack.pop()
+                continue
+            stack.append(char)
+        return "".join(stack)
+        
+        # stack = [S]
+        # while stack:
+        #     word = stack.pop()
+        #     if word == "":
+        #         break
+        #     last_letter = word[0]
+        #     for i in range(1,len(word)):
+        #         #iterate over all the letters to find a duplicate
+        #         #if a duplicate is found, remove it, add it to the stack
+        #         if word[i] == last_letter:
+        #             stack.append(word[:i-1]+word[i+1:])
+        #             break
+        #         last_letter = word[i]
+        #     if not stack:
+        #         #if after checking the word for duplicates, none is found, exit loop
+        #         break
+        # return word
                 
